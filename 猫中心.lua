c606b826b93aa9b36701b967a2c2e100ca3d4274
wcnm666663 <3262703519@qq.com>
2025-12-28 22:35:16 +0800

    Create 猫中心.lua with multiple hub tabs

---
+++ 猫中心.lua
@@ -0,0 +1,121 @@
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/v9Pdp6kx"))()
+local Window = OrionLib:MakeWindow({Name = "猫中心", HidePremium = false, SaveConfig = true,IntroText = "欢迎使用猫中心", ConfigFolder = "欢迎使用猫中心"})
+
+local Tab = Window:MakeTab({
+   Name = "公告 hub",
+   Icon = "rbxassetid://7733779610",
+   PremiumOnly = false
+})
+
+about:AddParagraph("popcat制作")
+
+Tab:AddButton({
+	Name = "缝合脚本请见谅",
+	Callback = function()
+      
+  	end    
+})
+
+Tab:AddButton({
+	Name = "qq群 928328623",
+	Callback = function()
+      
+  	end    
+})
+
+-- 示例 Tab 页面
+local Tab = Window:MakeTab({
+   Name = "其他的hub",
+   Icon = "rbxassetid://7733779610",
+   PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "XA HUB",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/h8nC0fLb", true))()loadstring(game:HttpGet("https://raw.githubusercontent.com/XingFork/Scripts/refs/heads/main/Ohio"))()      
+  	end    
+})
+
+Tab:AddButton({
+	Name = "禁漫中心",
+	Callback = function()
+getgenv().lishichuan="禁漫中心1001390385" loadstring(game:HttpGet("https://raw.githubusercontent.com/dingding123hhh/anlushanjinchangantangwanle/main/jmjmjmjmjmjmjmjmjmjmjmjmjmjmjmjm.lua"))()      
+  	end    
+})
+
+Tab:AddButton({
+	Name = "情云 hub",
+	Callback = function()
+loadstring(utf8.char((function() return table.unpack({108,111,97,100,115,116,114,105,110,103,40,103,97,109,101,58,72,116,116,112,71,101,116,40,34,104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,67,104,105,110,97,81,89,47,45,47,109,97,105,110,47,37,69,54,37,56,51,37,56,53,37,69,52,37,66,65,37,57,49,34,41,41,40,41})end)()))()      
+  	end    
+})
+
+Tab:AddButton({
+	Name = "翻译 hub",
+	Callback = function()
+TX = "TX Script"
+Script = "全自动翻译"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JsYb666/Item/refs/heads/main/Auto-language"))()      
+  	end    
+})
+
+Tab:AddButton({
+	Name = "落叶中心",
+	Callback = function()
+ getgenv().LS="落叶中心" loadstring(game:HttpGet("https://raw.githubusercontent.com/krlpl/Deciduous-center-LS/main/%E8%90%BD%E5%8F%B6%E4%B8%AD%E5%BF%83%E6%B7%B7%E6%B7%86.txt"))()     
+  	end    
+})
+
+-- 示例 Tab 页面
+local Tab = Window:MakeTab({
+   Name = "感染性微笑",
+   Icon = "rbxassetid://7733779610",
+   PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "hub 英文",
+	Callback = function()
+loadstring(game:HttpGet("https://rawscripts.net/raw/Infectious-Smile-CHRISTMAS-God-script-broo-75750"))()      
+  	end    
+})
+
+local Tab = Window:MakeTab({
+   Name = "最强战场",
+   Icon = "rbxassetid://7733779610",
+   PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "垃圾桶",
+	Callback = function()
+      
+  	end    
+})
+
+local Tab = Window:MakeTab({
+   Name = "木材大亨",
+   Icon = "rbxassetid://7733779610",
+   PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "木材大亨 英文",
+	Callback = function()
+ loadstring(game:HttpGet("https://rawscripts.net/raw/Lumber-Tycoon-2-Good-Scripts-Yuhhh-28846"))()     
+  	end    
+})
+
+local Tab = Window:MakeTab({
+   Name = "走路或死亡",
+   Icon = "rbxassetid://7733779610",
+   PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "Walk or Die 中文",
+	Callback = function()
+loadstring(game:HttpGet("https://rawscripts.net/raw/Walk-or-Die-Script-75015"))()      
+  	end    
+})
